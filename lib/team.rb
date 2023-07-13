e70130b1a0008e1a7147f43b2430c40357ac5edb
Nick Spencer <strangebrew202@gmail.com>
2023-07-13 09:59:52 -0600

    complete iteration3

--- lib/team.rb
+++ lib/team.rb
@@ -14,4 +14,29 @@ class Team
   def add_player(player)
     @roster << player
   end
+
+  def long_term_players
+    @roster.select do |player|
+      player.contract_length > 24
+    end
+  end
+
+  def short_term_players
+    @roster.select do |player|
+      player.contract_length <= 24
+    end
+  end
+
+  def total_value
+    @roster.reduce(0) do |sum, player|
+      sum + player.total_cost
+    end
+  end
+
+  def details
+    {
+      "total_value" => total_value,
+      "player_count" => player_count
+    }
+  end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -13,7 +13,7 @@ describe Team do
     end
   end
 
-  describe "#player_count" do
+  describe "#add_player, #player_count" do
 
     it "can add and count players on the team" do
       team = Team.new("Dodgers", "Los Angeles")
@@ -27,6 +27,75 @@ describe Team do
 
       expect(team.roster).to eq([player_1, player_2])
       expect(team.player_count).to eq(2)
+    end
+  end
+
+  describe "#long_term_players" do
+    it "can return array of only long term players" do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.long_term_players).to eq([player_1, player_3])
+      
+    end
+  end
+
+  describe "#short_term_players" do 
+    it "return an array with only short term players" do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.short_term_players).to eq([player_2, player_4])
+    end
+  end
+
+  describe "#total_value" do
+    it "returns total cost of team" do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.total_value).to eq(85200000)
+    end
+  end
+
+  describe "#details" do
+    it "return hash with total_value of team and player_count" do
+      team = Team.new("Dodgers", "Los Angeles")
+      player_1 = Player.new("Michael Palledorous" , 1000000, 36)
+      player_2 = Player.new("Kenny DeNunez", 500000, 24)
+      player_3 = Player.new("Alan McClennan", 750000, 48)
+      player_4 = Player.new("Hamilton Porter", 100000, 12)
+
+      team.add_player(player_1)
+      team.add_player(player_2)
+      team.add_player(player_3)
+      team.add_player(player_4)
+
+      expect(team.details).to eq({"total_value" => 85200000, "player_count" => 4})
     end
   end
 end
