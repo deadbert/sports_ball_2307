90b3305c3b92c16a482810aa8396ea7b3cc0ab18
Nick Spencer <strangebrew202@gmail.com>
2023-07-13 09:27:37 -0600

    Complete baseline iteration 1

--- spec/player_spec.rb
+++ spec/player_spec.rb
@@ -37,4 +37,17 @@ RSpec.describe Player do
 
     expect(player.total_cost).to eq(36000000)
   end
+
+  it 'initializes with a nickname equal to nil' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    expect(player.nickname).to eq(nil)
+  end
+
+  it 'can set a new nickname' do
+    player = Player.new("Michael Palledorous" , 1000000, 36)
+
+    player.set_nickname!("Squints")
+    expect(player.nickname).to eq("Squints")
+  end
 end
